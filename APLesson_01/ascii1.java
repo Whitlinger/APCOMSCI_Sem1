75059e01520c576f413dd7f0028a5011c9a2b435
Whitlinger <xuchelsea@gmail.com>
2016-09-02 14:20:54 -0700

    Added Files

---
+++ APLesson_01/ascii1.java
@@ -0,0 +1,15 @@
+public class firstProg
+{
+	public static void main(String[]args)
+	{
+		System.out.println("  , - ~ ~ -.___.");
+		System.out.println(" /  |      '           \\");
+		System.out.println("(   )                   0");
+		System.out.println(" \\_/ - ,  ,- - - - '");
+		System.out.println("      ====                            //");
+		System.out.println("     /     \\ - ' ~ ;             /~~~(0)");
+		System.out.println("    /     __/~ |          /               |");
+		System.out.println("=(  _______|     (__________|");
+	}
+}
+	
---
+++ APLesson_01/firstProg.java
@@ -0,0 +1,16 @@
+public class firstProg
+{
+	public static void main(String[]args)
+	{
+		System.out.println("        ___  ");
+		System.out.println(" ___/      \\___");
+		System.out.println("/      '- - -'       \\");
+		System.out.println("'- -_______- -'");
+		System.out.println("        /  \\");
+		System.out.println("       /    \\");
+		System.out.println("      / \\0/ \\");
+		System.out.println("      /   |   \\");
+		System.out.println("      /  / \\  \\");
+	}
+}
+	
