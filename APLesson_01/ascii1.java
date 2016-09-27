f909b0d2d88508248827a9ec3ca2a17d41296e98
Whitlinger <xuchelsea@gmail.com>
2016-09-27 13:06:31 -0700

    Edited files

--- APLesson_01/ascii1.java
+++ APLesson_01/ascii1.java
@@ -1,15 +1,15 @@
-public class firstProg
+public class ascii1
 {
 	public static void main(String[]args)
 	{
-		System.out.println("  , - ~ ~ -.___.");
-		System.out.println(" /  |      '           \\");
-		System.out.println("(   )                   0");
-		System.out.println(" \\_/ - ,  ,- - - - '");
-		System.out.println("      ====                            //");
-		System.out.println("     /     \\ - ' ~ ;             /~~~(0)");
-		System.out.println("    /     __/~ |          /               |");
-		System.out.println("=(  _______|     (__________|");
+		System.out.println("  ,-~~-.___.");
+		System.out.println(" / |   '    \\");
+		System.out.println("(  )         0");
+		System.out.println(" \\_/-, ,----'");
+		System.out.println("    ====             //");
+		System.out.println("   /  \\-'~;     /~~~(0)");
+		System.out.println("  /   __/~|   /        |");
+		System.out.println("=(  ______| (__________|");
 	}
 }
 	
