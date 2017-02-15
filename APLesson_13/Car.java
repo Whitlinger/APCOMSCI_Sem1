d73de234098d385091c037578c6b6da1fd505eaf
Whitlinger <xuchelsea@gmail.com>
2017-02-15 13:03:20 -0800

    Added Files

---
+++ APLesson_13/Car.java
@@ -0,0 +1,20 @@
+public class Car extends Toy
+{
+	private String name; 
+	
+	public Car()
+	{
+		super(); 
+	}
+	
+	public Car(String n)
+	{
+		super(n); 
+	}
+	
+	public String getType()
+	{
+		return "Car";
+	}
+	
+}
---
+++ APLesson_13/ToyRunner.java
@@ -0,0 +1,13 @@
+public class ToyRunner
+{
+	public static void main(String[]args)
+	{
+		
+		AFigure objectA = new AFigure("Batman");
+	
+		Car objectC = new Car("Batmobile"); 
+	
+		System.out.println(objectA + "\n" + objectC);
+	}
+	
+}
