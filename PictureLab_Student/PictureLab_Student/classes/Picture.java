6def82d6fbf3385fa128825c28313fe321670293
Whitlinger <xuchelsea@gmail.com>
2017-04-25 14:27:44 -0700

    Edited Files

--- PictureLab_Student/PictureLab_Student/classes/Picture.java
+++ PictureLab_Student/PictureLab_Student/classes/Picture.java
@@ -201,17 +201,24 @@ import java.util.List; // resolves problem with java.awt.List and java.util.List
   {
     Pixel leftPixel = null;
     Pixel rightPixel = null;
+    Pixel bottomPixel = null;
     Pixel[][] pixels = this.getPixels2D();
     Color rightColor = null;
-    for (int row = 0; row < pixels.length; row++)
+	Color bottomColor = null; 
+    for (int row = 0; row < pixels.length-1; row++)
     {
       for (int col = 0; 
            col < pixels[0].length-1; col++)
       {
         leftPixel = pixels[row][col];
         rightPixel = pixels[row][col+1];
+		bottomPixel = pixels[row+1][col]; 
         rightColor = rightPixel.getColor();
+		bottomColor = bottomPixel.getColor(); 
         if (leftPixel.colorDistance(rightColor) > 
+            edgeDist)
+          leftPixel.setColor(Color.BLACK);
+		else if (leftPixel.colorDistance(bottomColor) > 
             edgeDist)
           leftPixel.setColor(Color.BLACK);
         else
